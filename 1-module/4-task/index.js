5f67ba427acefc2f4285e474d88fc600db980b97
Vitalii Maevskii <v89111916010@gmail.com>
2022-01-26 22:22:55 +0300

    1-module 4-task # Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  if (~str.toLowerCase().indexOf('xxx') || ~str.toLowerCase().indexOf('1xbet')) return true;
+
+  // or
+  // if (str.toLowerCase().includes('xxx') || str.toLowerCase().includes('1xbet')) return true;
+
+  else return false;
 }
