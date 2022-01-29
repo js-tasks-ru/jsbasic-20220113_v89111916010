4f447083c03c4ef06b4584b828d295453b4c888c
Vitalii Maevskii <v89111916010@gmail.com>
2022-01-29 10:12:31 +0300

    1-module 5-task # Усечение строки ## Возврат значения

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length <= maxlength) {
+    return str;
+  } else {
+    let newStr;
+    newStr = str.substr(0, maxlength - 1) + "…";
+    return newStr;
+  }
 }
+
+truncate();
