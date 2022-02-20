1a74ca6e0b548c5eae140d1f9f58b21f8b3b0b7a
Vitalii Maevskii <v89111916010@gmail.com>
2022-02-20 23:20:33 +0300

    3-module 5-task Найти min/max в строке Результат функция должна возвращать в виде объекта

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,23 @@
 function getMinMax(str) {
-  // ваш код...
+  const dumpArr = str.split(' ');
+
+  const clearArr = dumpArr.filter(item => {
+    const transformItem = +item;
+
+    if (isFinite(transformItem)) {
+      return transformItem
+    }
+  });
+
+  const result = {};
+  let minValue;
+  let maxValue;
+
+  minValue = Math.min(...clearArr);
+  maxValue = Math.max(...clearArr);
+
+  result.min = minValue;
+  result.max = maxValue;
+  return result;
+
 }
